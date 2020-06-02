4f7d21bb3ca2a9ffe4f3f56e25c6361da6b97015
Jacob Haff <jacobhaff2@gmail.com>
2020-06-02 19:03:00 -0400

    Initial Commit

---
+++ Would-You-Rather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Would-You-Rather
+//
+//  Created by Jacob Haff on 6/2/20.
+//  Copyright © 2020 Jacob Haff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
