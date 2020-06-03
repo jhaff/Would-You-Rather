f23cd7a20084e98c1eea7b2075a1c9abad39f1ee
Jacob Haff <jacobhaff2@gmail.com>
2020-06-03 16:03:50 -0400

    Implement basic quiz functinoality with 2d array

--- Would-You-Rather/ViewController.swift
+++ Would-You-Rather/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButtoon: UIButton!
+    
+    let quiz = [
+        ["a dog barks", "True"],
+        ["a cow moos", "True"],
+        ["a cat growls", "False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
+    @IBAction func answerButtonPressd(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("right")
+        } else {
+            print("wrong")
+        }
+        
+        if questionNumber < quiz.count - 1{
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        updateUI()
+    }
 
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
 }
 
